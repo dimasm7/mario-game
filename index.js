a7cde076138aeccea81686f96368d6083313af9b
dimasm7 <dimasmaulana.dm89@gmail.com>
2022-02-13 17:41:22 +0700

    player movement

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ class Player {
     update(){
         this.draw()
         this.position.y += this.velocity.y
+        this.position.x += this.velocity.x
         
         if(this.position.y + this.velocity.y + this.height <= canvas.height){
             this.velocity.y += gravity
@@ -36,10 +37,66 @@ class Player {
 }
 
 const player = new Player()
+const keys = {
+    right:{
+        pressed: false
+    },
+    left:{
+        pressed: false
+    },
+}
 
 function animate(){
     requestAnimationFrame(animate)
     c.clearRect(0, 0, canvas.width, canvas.height)
     player.update()
+
+    if(keys.right.pressed){
+        player.velocity.x = 5
+    }else if (keys.left.pressed) {
+        player.velocity.x = -5
+    } else{
+        player.velocity.x = 0
+    }
 }
 animate()
+
+addEventListener('keydown', ({ keyCode }) => {
+    switch (keyCode) {
+        case 65:
+            keys.left.pressed = true
+            break;
+        case 83:
+            console.log('down')
+            break;
+        case 68:
+            keys.right.pressed = true
+            break;
+        case 87:
+            player.velocity.y -= 20
+            break;
+        
+        default:
+            break;
+    }
+})
+
+addEventListener('keyup', ({ keyCode }) => {
+    switch (keyCode) {
+        case 65:
+            keys.left.pressed = false
+            break;
+        case 83:
+            console.log('down')
+            break;
+        case 68:
+            keys.right.pressed = false
+            break;
+        case 87:
+            player.velocity.y -= 20
+            break;
+    
+        default:
+            break;
+    }
+})
