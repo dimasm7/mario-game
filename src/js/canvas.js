cc335391c07c7645da603f3ffebebcc6f13d6606
dimasm7 <dimasmaulana.dm89@gmail.com>
2022-03-20 15:37:07 +0700

    image platform

--- src/js/canvas.js
+++ src/js/canvas.js
@@ -1,7 +1,9 @@
+import platform from '../img/platform.png'
+
 const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
-canvas.width = window.innerWidth
-canvas.height = window.innerHeight
+canvas.width = 1024
+canvas.height = 567
 
 const gravity = 1.5
 class Player {
@@ -37,23 +39,38 @@ class Player {
 }
 
 class Platform {
-    constructor({x, y}){
+    constructor({x, y, image}){
         this.position = {
             x,
             y
         }
-        this.width = 200
-        this.height = 20
+        this.image = image
+        this.width = image.width
+        this.height = image.height
+
     }
 
     draw(){
-        c.fillStyle = 'blue'
-        c.fillRect(this.position.x, this.position.y, this.width, this.height)
+        c.drawImage(this.image, this.position.x, this.position.y)
     }
 }
 
+const image = new Image()
+image.src = platform
+
 const player = new Player()
-const platforms = [new Platform({x: 200, y: 100}),  new Platform({x: 500, y: 200})]
+const platforms = [
+    new Platform({
+        x: -1, 
+        y: 470, 
+        image
+    }),  
+    new Platform({
+        x: image.width -3, 
+        y: 470,
+        image
+    })
+]
 
 const keys = {
     right:{
@@ -67,11 +84,12 @@ const keys = {
 let scrollOffset = 0
 function animate(){
     requestAnimationFrame(animate)
-    c.clearRect(0, 0, canvas.width, canvas.height)
-    player.update()
+    c.fillStyle = 'white'
+    c.fillRect(0, 0, canvas.width, canvas.height)
     platforms.forEach((platform) => {
         platform.draw()
     })
+    player.update()
 
     if(keys.right.pressed && player.position.x < 400){ //maju
         player.velocity.x = 5
